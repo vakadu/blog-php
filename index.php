ec2a999adc52ce4a4d911cfdf647370aa191af69
vinod10 <vinodkumar@kwiqpick.com>
2017-06-29 16:23:20 +0530

    completed blog fixing some bugs

---
+++ index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="en">
+<head>
+    <title>Kwiqpick | Blog</title>
+    <!-- Kwiqpick Blog Page -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Kwiqpick - Responsive Blog Landing Page">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" lang="en" content="Kwiqpick - Responsive Blog Landing Page">
+    <meta name="keywords" lang="en" content="Kwiqpick, blog, landing blog page,
+    mobile-landing page, responsive, responsive blog page,
+    fully responsive, Orbit-app landing page">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <!--Custom Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,400i,700" rel="stylesheet">
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <!--Kwiqpick blog css-->
+    <link rel="stylesheet" href="css/blog.css">
+</head>
+<body>
+
+<header id="header">
+    <div class="nav-container">
+        <div class="container">
+            <div class="row">
+                <nav class="main-navigation">
+                    <div class="col-md-3">
+                        <div class="kwiqpick-logo">
+                            <a href="index.php">
+                                <img src="images/Kwiqpick_Logo_CB202D.png" alt="Kwiqpick">
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="articles">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-8">
+                <article class="article-item">
+                    <div class="enter-media">
+                        <div class="article-heading">
+                            <h2>
+                                <a href="post.php">6 Restaurants In Hyderabad Geetha, An Avid Swiggy User, Loves To Order From Frequently!</a>
+                            </h2>
+                            <div class="enter-date">
+                                <ul>
+                                    <li><a href="javascript:void(0)"> <i class="fa
+                                    fa-calendar">&nbsp;&nbsp;28-06-2017</i></a> </li>
+                                    <li><a href="#"><i class="fa
+                                    fa-user">&nbsp;</i> Team Kwiqpick</a> </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="article-body">
+                            <img src="images/blog-header-1-750x410.jpg" alt="Image">
+                            <p>
+                                Team Swiggy spoke to Geetha, a Swiggy super user in Hyderabad’s twin city, Secunderabad. Geetha was introduced to Swiggy by a friend, and she’s never looked back since. In fact, when we first spoke to her, a Hunger Savior was at her doorstep, delivering her order!   “Swiggy is popular in Hyderabad because of me!” […]
+                            </p>
+                        </div>
+                        <div class="article-footer">
+                            <div class="row">
+                                <div class="col-md-5">
+                                    <a href="post.php" class="button blog-button">Read
+                                        More</a>
+                                </div>
+                                <div class="col-md-7">
+                                    <div class="share-social-article">
+                                        <a href="javascript:;"><i class="fa
+                                        fa-facebook"></i></a>
+                                        <a href="javascript:;"><i class="fa
+                                        fa-linkedin"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article class="article-item">
+                    <div class="enter-media">
+                        <div class="article-heading">
+                            <h2>
+                                <a href="post.php">6 Restaurants In Hyderabad Geetha, An Avid Swiggy User, Loves To Order From Frequently!</a>
+                            </h2>
+                            <div class="enter-date">
+                                <ul>
+                                    <li><a href="javascript:void(0)"> <i class="fa
+                                    fa-calendar">&nbsp;&nbsp;28-06-2017</i></a> </li>
+                                    <li><a href="#"><i class="fa
+                                    fa-user">&nbsp;</i> Team Kwiqpick</a> </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="article-body">
+                            <img src="images/blog-header-1-750x410.jpg" alt="Image">
+                            <p>
+                                Team Swiggy spoke to Geetha, a Swiggy super user in Hyderabad’s twin city, Secunderabad. Geetha was introduced to Swiggy by a friend, and she’s never looked back since. In fact, when we first spoke to her, a Hunger Savior was at her doorstep, delivering her order!   “Swiggy is popular in Hyderabad because of me!” […]
+                            </p>
+                        </div>
+                        <div class="article-footer">
+                            <div class="row">
+                                <div class="col-md-5">
+                                    <a href="post.php" class="button blog-button">Read
+                                        More</a>
+                                </div>
+                                <div class="col-md-7">
+                                    <div class="share-social-article">
+                                        <a href="javascript:;"><i class="fa
+                                        fa-facebook"></i></a>
+                                        <a href="javascript:;"><i class="fa
+                                        fa-linkedin"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article class="article-item">
+                    <div class="enter-media">
+                        <div class="article-heading">
+                            <h2>
+                                <a href="post.php">6 Restaurants In Hyderabad Geetha, An Avid Swiggy User, Loves To Order From Frequently!</a>
+                            </h2>
+                            <div class="enter-date">
+                                <ul>
+                                    <li><a href="javascript:void(0);"> <i class="fa
+                                    fa-calendar">&nbsp;&nbsp;28-06-2017</i></a> </li>
+                                    <li><a href="#"><i class="fa
+                                    fa-user">&nbsp;</i> Team Kwiqpick</a> </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="article-body">
+                            <img src="images/blog-header-1-750x410.jpg" alt="Image">
+                            <p>
+                                Team Swiggy spoke to Geetha, a Swiggy super user in Hyderabad’s twin city, Secunderabad. Geetha was introduced to Swiggy by a friend, and she’s never looked back since. In fact, when we first spoke to her, a Hunger Savior was at her doorstep, delivering her order!   “Swiggy is popular in Hyderabad because of me!” […]
+                            </p>
+                        </div>
+                        <div class="article-footer">
+                            <div class="row">
+                                <div class="col-md-5">
+                                    <a href="post.php" class="button blog-button">Read
+                                        More</a>
+                                </div>
+                                <div class="col-md-7">
+                                    <div class="share-social-article">
+                                        <a href="javascript:void(0);"><i class="fa
+                                        fa-facebook"></i></a>
+                                        <a href="javascript:void(0);"><i class="fa
+                                        fa-linkedin"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <div class="article-pagination">
+                    <ul>
+                        <li><span>1</span></li>
+                        <li><a href="javascript:;">2</a></li>
+                        <li><a href="javascript:;">3</a></li>
+                        <li><a href="javascript:;"> <i class="fa fa-chevron-right"></i>
+                            </a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-4 blog-sidebar">
+                <div class="blog-widget">
+                    <div class="well">
+                        <h3>Search</h3>
+                        <form action="" method="post">
+                            <div class="input-group">
+                                <input type="text" name="search" class="form-control"
+                                       placeholder="Search">
+                                <span class="input-group-btn">
+                                    <button name="submit" class="btn btn-search"
+                                            type="submit">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="blog-widget color-widget">
+                    <h3>Subscribe</h3>
+                    <p>Sign up to receive updates and latest new things from us everyday.
+                        And i will not spam promise.</p>
+                    <div class="widget-forms">
+                        <form class="subscribe-form">
+                            <input type="text" placeholder="Subscribe">
+                            <a href="" class="sidebar-button button-color">Subscribe</a>
+                        </form>
+                    </div>
+                </div>
+<!--                <div class="blog-widget">-->
+<!--                    <h3>Popular Posts</h3>-->
+<!--                </div>-->
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <nav>
+                    <ul class="list-unstyled list-inline">
+                        <li><a href="">Careers</a> </li>
+                        <li><a href="">Contact Us</a> </li>
+                        <li><a href="">FAQ</a> </li>
+                        <li><a href="">Privacy Policy</a> </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- Back to top Section Begins -->
+    <a href="javascript:void(0);" class="back-to-top hidden-xs hidden-sm"><i class="fa
+    fa-angle-up" aria-hidden="true"></i></a>
+    <!-- Back to top Section Ends -->
+</footer>
+
+<script src="js/jquery-3.2.1.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/SmoothScroll.js"></script>
+<script src="js/blog.js"></script>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="en">
+<head>
+    <title>Kwiqpick | Blog</title>
+    <!-- Kwiqpick Blog Page -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Kwiqpick - Responsive Blog Landing Page">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" lang="en" content="Kwiqpick - Responsive Blog Landing Page">
+    <meta name="keywords" lang="en" content="Kwiqpick, blog, landing blog page,
+    mobile-landing page, responsive, responsive blog page,
+    fully responsive, Orbit-app landing page">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <!--Custom Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,400i,700" rel="stylesheet">
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <!--Kwiqpick blog css-->
+    <link rel="stylesheet" href="css/blog.css">
+</head>
+<body>
+
+<header id="header">
+    <div class="nav-container">
+        <div class="container">
+            <div class="row">
+                <nav class="main-navigation">
+                    <div class="col-md-3">
+                        <div class="kwiqpick-logo">
+                            <a href="index.php">
+                                <img src="images/Kwiqpick_Logo_CB202D.png" alt="Kwiqpick">
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="articles">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-8">
+                <article class="article-item">
+                    <div class="enter-media">
+                        <div class="article-heading">
+                            <h2>
+                                <a href="post.php">6 Restaurants In Hyderabad Geetha, An Avid Swiggy User, Loves To Order From Frequently!</a>
+                            </h2>
+                            <div class="enter-date">
+                                <ul>
+                                    <li><a href="javascript:void(0)"> <i class="fa
+                                    fa-calendar">&nbsp;&nbsp;28-06-2017</i></a> </li>
+                                    <li><a href="#"><i class="fa
+                                    fa-user">&nbsp;</i> Team Kwiqpick</a> </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="article-body">
+                            <img src="images/blog-header-1-750x410.jpg" alt="Image">
+                            <p>
+                                Team Swiggy spoke to Geetha, a Swiggy super user in Hyderabad’s twin city, Secunderabad. Geetha was introduced to Swiggy by a friend, and she’s never looked back since. In fact, when we first spoke to her, a Hunger Savior was at her doorstep, delivering her order!
+                            </p>
+                            <div class="para-heading">
+                                <h4>“Swiggy is popular in Hyderabad because of me!”</h4>
+                            </div>
+                            <p>
+                                After trying a variety of restaurants and apps, Geetha knows her favourite eateries, eats & the Swiggy app like the back of her hand. She also claims that she’s responsible for Swiggy’s popularity in her circles (and we definitely agree with her!).
+                            </p>
+                            <p>
+                                Knowing her love for food & the Swiggy expert that she is, we wanted to pick her brain on the best eats in her area and beyond. Curious to know her Hyderabad favourites? Read on to find out!
+                            </p>
+                        </div>
+                        <div class="article-footer">
+                            <div class="row">
+                                <div class="col-md-5 blog-tags">
+                                    <a href="javascript:;">pizza</a>
+                                    <a href="javascript:;">food</a>
+                                    <a href="javascript:;">biriyani</a>
+                                </div>
+                                <div class="col-md-7">
+                                    <div class="share-social-article">
+                                        <a href="http://www.facebook.com/sharer.php?u=http://www.kwiqpick.com/business" target="_blank"><i class="fa fa-facebook"></i></a>
+                                        <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=http://www.kwiqpick.com" target="_blank""><i class="fa fa-linkedin"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article class="article-item">
+                    <div class="enter-media">
+                        <div class="article-heading">
+                            <h2>
+                                Leave a Reply
+                            </h2>
+                        </div>
+                        <div class="article-body article-comments">
+                            <form>
+                                <div class="comment-form ">
+                                    <p class="input-name"> NAME <span>*</span> </p>
+                                    <input type="text" name="name" placeholder="">
+                                    <p class="input-name"> E-MAIL <span>*</span> </p>
+                                    <input type="text" name="name" placeholder="">
+                                    <p class="input-name"> COMMENT</p>
+                                    <textarea placeholder=""></textarea>
+                                </div>
+                                <div class="comment-submit">
+                                    <a href="javascript:;" class="custom-buttons
+                                    button-color">Post Comment</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </article>
+            </div>
+
+            <div class="col-md-4 blog-sidebar">
+                <div class="blog-widget">
+                    <div class="well">
+                        <h3>Search</h3>
+                        <form action="" method="post">
+                            <div class="input-group">
+                                <input type="text" name="search" class="form-control"
+                                       placeholder="Search">
+                                <span class="input-group-btn">
+                                    <button name="submit" class="btn btn-search"
+                                            type="submit">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="blog-widget color-widget">
+                    <h3>Subscribe</h3>
+                    <p>Sign up to receive updates and latest new things from us everyday.
+                        And i will not spam promise.</p>
+                    <div class="widget-forms">
+                        <form class="subscribe-form">
+                            <input type="text" placeholder="Subscribe">
+                            <a href="" class="sidebar-button button-color">Subscribe</a>
+                        </form>
+                    </div>
+                </div>
+                <div class="blog-widget">
+                    <h3 class="hasMargin">Tags</h3>
+                    <div class="widget-body">
+                        <div class="widget-tags">
+                            <a href="javascript:;">Blog</a>
+                            <a href="javascript:;">Pizza</a>
+                            <a href="javascript:;">Burger</a>
+                            <a href="javascript:;">Biriyani</a>
+                            <a href="javascript:;">Momo</a>
+                            <a href="javascript:;">Rasagulla</a>
+                            <a href="javascript:;">Rolls</a>
+                            <a href="javascript:;">Meals</a>
+                            <a href="javascript:;">Chicken Curry</a>
+                            <a href="javascript:;">Enjoy</a>
+                            <a href="javascript:;">Ramen</a>
+                            <a href="javascript:;">Unagi</a>
+                            <a href="javascript:;">Food</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <nav>
+                    <ul class="list-unstyled list-inline">
+                        <li><a href="">Careers</a> </li>
+                        <li><a href="">Contact Us</a> </li>
+                        <li><a href="">FAQ</a> </li>
+                        <li><a href="">Privacy Policy</a> </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- Back to top Section Begins -->
+    <a href="javascript:void(0);" class="back-to-top hidden-xs hidden-sm"><i class="fa
+    fa-angle-up" aria-hidden="true"></i></a>
+    <!-- Back to top Section Ends -->
+</footer>
+
+<script src="js/jquery-3.2.1.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/SmoothScroll.js"></script>
+<script src="js/blog.js"></script>
+</body>
+</html>
