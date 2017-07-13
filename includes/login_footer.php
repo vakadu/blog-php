5917cbfaa6178a6350a313fdfff9f54d3265543b
vinod10 <vinodkumar@kwiqpick.com>
2017-07-13 16:53:01 +0530

    removed login in footer

--- includes/login_footer.php
+++ includes/login_footer.php
@@ -1,10 +1,3 @@
-
-<footer>
-    <div class="container">
-        <p class="pull-right">Login, 2017. Don't copy, it's Bad Karma.</p>
-    </div>
-</footer>
-
 </section>
 
 <script src="../js/jquery-3.2.1.js"></script>
